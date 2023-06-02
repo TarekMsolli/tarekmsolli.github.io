53d49fb1677e44a181ffe37b2fee96e6f8e29dd1
Tarek <tarekmsolli@gmail.com>
2023-06-02 02:15:05 +0100

    added some basic info about myself

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+const CVTOGGLEBUTTON = document.getElementById('cv-toggleButton');
+const CV = document.getElementById('cv');
+
+function toggleCV (){
+  CV.classList.toggle('popped');
+}
+// This function displays current time HH:MM and date DD/MM/YYYY seperated into 2 divs 
 function displayDateTime() {
     var timeElement = document.getElementById('time');
     var dateElement = document.getElementById('date');
@@ -18,14 +25,19 @@ function displayDateTime() {
     var formattedTime = currentDate.toLocaleTimeString(undefined, timeOptions);
     var formattedDate = currentDate.toLocaleDateString(undefined, dateOptions);
 
+    // By default the date function uses MM/DD/YYYY so we have to rearrange them like this
     var dateItems = formattedDate.split('/');
     formattedDate = dateItems[1] + '/' + dateItems[0] + '/' + dateItems[2];
   
     timeElement.textContent = formattedTime;
     dateElement.textContent = formattedDate;
   }
+
+
   
+  // I have to run the function once before the interval begins or else the time and date wont show until the cycle runs once
   displayDateTime();
   
   setInterval(displayDateTime, 1000);
-  
+
+  CVTOGGLEBUTTON.addEventListener('click', toggleCV)
